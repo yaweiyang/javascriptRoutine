f12477b77536e84d32dd82099a65d18bb8336def
mr_yang_xiaoyi <991575693@qq.com>
2019-05-05 23:28:16 +0800

    数组部分方法的应用

--- VariablesAndDataStructures/arrayFunc.js
+++ VariablesAndDataStructures/arrayFunc.js
@@ -7,6 +7,7 @@ let arr3 = [7, 8, 9];
 let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let arr6 = [1, 2, 3, 2, 1, 3];
+let arr7 = [{ name:'yangyawei', score:100}, { name:'yangmeili', score:105}, { name:'xiaohong', score:23}, { name:'xiaoming', score:60}, { name:'xiaobai', score:80}];
 
 let isEven = function (x) {
     return (x % 2 === 0);
@@ -37,5 +38,30 @@ let isEven1 = function (x) {
 // arr4.reverse();
 // let arr = arr4;
 
+// 对数组进行排序 没有返回值 排序的方法可以自定义
+// arr6.sort();
+// let arr = arr6;
+// arr7.sort((a,b)=>{
+//     if (a.score >= b.score){
+//         return 1;
+//     } else{
+//         return -1;
+//     }
+// });
+// console.log(arr7);
+
+// 以数组的形式返回指定范围内的数组
+// let arr = arr4.slice(4, 7);
+
+// 向数组中添加、删除、替换元素 并返回被删除的项目
+// let arr = arr4.splice(1, 0, 'a');
+
+// let arr =arr4.splice(1,1,'a');
+
+// let arr = arr4.splice(1,2, 'a');// 第三个参数不存在时删除元素不添加元素
+// console.log(arr4);
+
 
 console.log(arr);
+
+
