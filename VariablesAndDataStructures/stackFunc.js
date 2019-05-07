17d120dcb085896176fcd896074c388626fc8446
mr_yang_xiaoyi <991575693@qq.com>
2019-05-07 16:17:01 +0800

    栈 ES6语法

---
+++ VariablesAndDataStructures/stackFunc.js
@@ -0,0 +1,56 @@
+class Stack {
+    constructor () {
+        this.items = [];
+    }
+
+    /**
+     * 向栈顶添加元素
+     * @param element
+     */
+    push (element) {
+        this.items.push(element);
+    }
+
+    /**
+     * 移除栈顶元素
+     * @param element
+     * @returns {*}
+     */
+    pop (element) {
+        return this.items.pop();
+    }
+
+    /**
+     * 返回栈顶元素
+     * @returns {*}
+     */
+    peek () {
+        return this.items[this.items.length-1];
+    }
+
+    /**
+     * 判断栈中是否有元素
+     * @returns {boolean}
+     */
+    isEmpty () {
+        return this.items.length === 0;
+    }
+
+    /**
+     * 返回栈中有多少元素
+     * @returns {number}
+     */
+    size () {
+        return this.items.length;
+    }
+    clear () {
+        this.items = [];
+    }
+    print () {
+        console.log(this.items.toString());
+    }
+}
+let stack = new Stack();
+console.log(stack.isEmpty());
+
+export {stack}
