1f6df3bc86592c7c5cf9fbda1677363f5a35cc02
mr_yang_xiaoyi <991575693@qq.com>
2019-05-08 00:03:43 +0800

    数组查重并删除重合的元素至一个

--- insterestingFunc.js
+++ insterestingFunc.js
@@ -1,8 +1,6 @@
 /**
  * 每一秒输出一个从 0~4 的数字
  */
-
-
 function alarm() {
     // 原本的for循环
     /*for (var i = 0; i < 5; i++) {
@@ -29,3 +27,28 @@ function alarm() {
 }
 
 alarm();
+
+/**
+ * 查找数组中相同的元素并删除至一个
+ * @param arr
+ * @returns {*}
+ */
+function deleteSameElementInArray(arr) {
+    console.log(arr);
+    if(arr.length <= 1)
+        return arr;
+    let curIndex = 0;
+    while (curIndex < arr.length-1) {
+        let curElement = arr[curIndex];
+        let lastIndex = arr.lastIndexOf(curElement);
+        // console.log('数组中存在多个元素%s，位置分别为：%d  %t',curElement,curIndex,lastIndex);
+        if (lastIndex === curIndex) {
+            curIndex ++;
+        } else {
+            arr.splice(lastIndex, 1);
+        }
+    }
+    return arr;
+}
+
+console.log(deleteSameElementInArray([1,3,5,7,9,11,20,3,6,8,9,13,12,3,5,20,78,56,12,23]));
