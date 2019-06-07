35a0c47a4814df8adcc5e1721caf04e2b0180a01
yangyawei <hugehorse@sina.cn>
2019-06-07 23:45:40 +0800

    es5  priorityQueue 优先队列的定义

--- VariablesAndDataStructures/queue/ES5Queue.js
+++ VariablesAndDataStructures/queue/ES5Queue.js
@@ -1,4 +1,7 @@
-// 队列 先进先出
+/**
+ * 队列 先进先出
+ * @constructor
+ */
 function MyQueue () {
 
     let items = [];
@@ -26,9 +29,75 @@ function MyQueue () {
     this.size = function () {
         return items.length;
     };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
 }
 
 let que = new MyQueue();
 console.log(que.isEmpty());
 que.enqueue('yangyawei');
 console.log(que.front());
+que.enqueue('yangmeili');
+que.print();
+console.log(que.dequeue());
+que.print();
+
+/**
+ * 优先队列
+ * @constructor
+ */
+function MyPriorityQueue() {
+
+    let items = [];
+
+    function QueueElement (element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+
+    this.enqueue = function (element, priority) {
+        let queueElement = new QueueElement(element, priority);
+
+        if (this.isEmpty()) {
+            items.push(queueElement);
+        } else {
+            var added = false;
+            for (let i = 0; i < items.length; ++i) {
+                if (queueElement.priority > items[i].priority) {
+                    items.splice(i, 1, queueElement);
+                    added = true;
+                    break;
+                }
+             }
+             if (!added) {
+                items.push(queueElement);
+             }
+        }
+    };
+
+    this.dequeue = function () {
+        return items.shift();
+    };
+
+    this.front = function () {
+        return items[0];
+    };
+
+    this.isEmpty = function () {
+        return 0 == items.length;
+    }
+
+    this.clear = function () {
+        items = [];
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
+}
